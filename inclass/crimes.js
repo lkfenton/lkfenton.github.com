e4da4291d5cde119a21525f74196d8dbea78eaac
leah fenton <leah.fenton@massart.edu>
2013-04-17 17:36:45 -0400

    inclass stuff

---
+++ inclass/crimes.js
@@ -0,0 +1,14 @@
+$(function() {
+    
+  var i = 0;
+  while (i < 100) {
+          
+  var crime = crimes.data[i];
+          
+  var crimeCode = code[42];
+          
+  console.log(crimeCode);
+  
+  i++;
+        }
+});
