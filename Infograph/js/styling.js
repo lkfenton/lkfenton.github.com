edb6252fd2ad0ff8fe1f8178ab1d740cefeec7bb
leah fenton <leah.fenton@massart.edu>
2013-05-01 17:20:03 -0400

    Styling

---
+++ Infograph/js/styling.js
@@ -0,0 +1,59 @@
+Highcharts.setOptions({
+    chart: {
+        style: {
+            fontFamily: 'Abel',
+            color: "#000000"
+        }
+
+    },
+    title: {
+      style: {
+         color: 'gray',
+         font: '25px "Abel", Abel, sans-serif',
+       
+      }
+   },
+    xAxis: {
+      gridLineWidth: 1,
+      lineColor: 'gray',
+      tickColor: 'gray',
+      labels: {
+         style: {
+            color: 'gray',
+            font: '12px Abel, Abel, sans-serif'
+         }
+      },
+      title: {
+         style: {
+            color: 'gray',
+            fontWeight: 'bold',
+            fontSize: '12px',
+            fontFamily: 'Buenard, Buenard, serif'
+
+         }            
+      }
+   },
+   yAxis: {
+      minorTickInterval: 'auto',
+      lineColor: '#000',
+      lineWidth: 1,
+      tickWidth: 1,
+      tickColor: '#000',
+      labels: {
+         style: {
+            color: 'gray',
+            font: '12px Trebuchet MS, Verdana, sans-serif'
+         }
+      },
+      title: {
+      	
+         style: {
+            color: 'gray',
+            fontSize: '18px',
+            fontFamily: 'Abel, Abel, sans-serif'
+         }            
+      }
+   },
+   
+
+});
